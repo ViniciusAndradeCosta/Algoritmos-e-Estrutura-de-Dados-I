7891da97c3ded9200eb6c66a74d0d77c8a9a5ecc
Vinícius Andrade <135062180+ViniciusAndradeCosta@users.noreply.github.com>
2024-02-09 21:09:13 -0300

    Update interface.h

--- TAD FILA/interface.h
+++ TAD FILA/interface.h
@@ -1,1 +1,16 @@
+# ifndef INTERFACE_H_INCLUDED
+# define INTERFACE_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fila.h"
+
+void MSG_MENU();// MENSAGEM PARA EXIBIR O MENU PRINCIPAL
+
+void MSG_FILA();
+
+void MENU(TFila *fila, TFila *fila2, TFila *fila3); // MÓDULO DE MENU
+
+# endif // INTERFACE_H_INCLUDED
 
